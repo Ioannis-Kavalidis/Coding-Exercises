119f95896572b7ef999fb3eee311f1fa00b9a252
paperjar <dreamtnem@gmail.com>
2020-02-20 16:21:45 +0100

    more revision exrecises

--- live-coding/02-20 February/20-02/index.js
+++ live-coding/02-20 February/20-02/index.js
@@ -102,7 +102,7 @@ console.log(array.some(even));
 
 /* create a function called verbing. It should take a single argument, a string. 
 If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. 
-If the string length is less than 3, it should leave it unchanged. For example:
+If the string length is less than 3, it should leave it unchanged. For example
 
 verbing('swim'): 'swimming'
 verbing('swimming'): 'swimmingly'
