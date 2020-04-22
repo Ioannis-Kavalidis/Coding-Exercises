95942955c9eb763113153dd0353b84e462ba16f7
paperjar <dreamtnem@gmail.com>
2020-04-22 11:17:14 +0200

    More DOM

--- live-coding/04-20 April/22-04/index.js
+++ live-coding/04-20 April/22-04/index.js
@@ -16,3 +16,12 @@ const addToList = () => {
   document.querySelector(".result").appendChild(newLi);
   document.querySelector("#userText").value = "";
 };
+
+const addLastName = () => {
+  let userInput2 = document.querySelector("#userText2").value;
+  let newLi = document.createElement("li");
+  let text = document.createTextNode(userInput2);
+  newLi.appendChild(text);
+  document.querySelector(".return").appendChild(newLi);
+  document.querySelector("#userText2").value = "";
+};
