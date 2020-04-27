03128c59ba3c64ef47f1a3ae813e3d82e7ef13ca
paperjar <dreamtnem@gmail.com>
2020-04-27 10:27:39 +0200

    <more Node

---
+++ live-coding/04-20 April/27-04/index.js
@@ -0,0 +1,10 @@
+// Traversing
+//  Node vs Element Nodes
+let ele = document.querySelector("#second").previousElementSibling;
+console.log(ele);
+// console.log(ele.id);
+let ele2 = document.querySelector("body").previousSibling;
+console.log(ele2);
+// first Child
+let mainText = document.getElementById("text").firstElementChild;
+console.log(mainText.textContent);
