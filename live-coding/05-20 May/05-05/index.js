67de448a1f5ac28119e91d332cb1740f70fdba53
paperjar <dreamtnem@gmail.com>
2020-05-05 10:12:25 +0200

    Promises

--- live-coding/05-20 May/05-05/index.js
+++ live-coding/05-20 May/05-05/index.js
@@ -9,6 +9,8 @@ window.setTimeout(() => {
 // Non-blocking code
 
 let checkPromise = new Promise((resolve, reject) => {
+  // this is the object
+  // declaring the promise
   // resolve illustrates the success part and reject the failure part
   let x = false;
   if (x) {
@@ -18,10 +20,28 @@ let checkPromise = new Promise((resolve, reject) => {
   }
 });
 
-checkPromise
+checkPromise // then and catch are methods
+  // calling the promise
   .then((message) => {
     console.log(`I am then and ${message}`); // then is the resolve from above
   })
   .catch((message) => {
     console.log(`I am catch.  ${message}`); // catch is the reject from above
   });
+
+//
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+let ms = 3000;
+delay(ms).then(myFunction());
+
+function myFunction() {
+  let text = document.querySelector(".text");
+  text.innerHTML = `It took me ${ms} ms to be done`;
+}
