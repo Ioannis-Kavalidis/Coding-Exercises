659791ec1d9bb2a285d87f8637acb570fa70aa11
paperjar <dreamtnem@gmail.com>
2020-05-05 09:25:15 +0200

    Promises

--- live-coding/05-20 May/05-05/index.js
+++ live-coding/05-20 May/05-05/index.js
@@ -2,4 +2,26 @@
 
 // Blocking code
 
+window.setTimeout(() => {
+  //   alert("Hi");
+}, 10000);
+
 // Non-blocking code
+
+let checkPromise = new Promise((resolve, reject) => {
+  // resolve illustrates the success part and reject the failure part
+  let x = false;
+  if (x) {
+    resolve("Everything is well");
+  } else {
+    reject("Sorry but something went wrong");
+  }
+});
+
+checkPromise
+  .then((message) => {
+    console.log(`I am then and ${message}`); // then is the resolve from above
+  })
+  .catch((message) => {
+    console.log(`I am catch.  ${message}`); // catch is the reject from above
+  });
