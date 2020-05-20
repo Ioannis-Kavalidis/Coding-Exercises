a3590a505093af79d497b640a26b6b4440c98141
paperjar <dreamtnem@gmail.com>
2020-05-20 09:40:39 +0200

    Revision on Callback Functions

--- live-coding/05-20 May/20-05/index.js
+++ live-coding/05-20 May/20-05/index.js
@@ -1,3 +1,37 @@
 // callbacks repetition
 
 // let's make hot chocolate
+
+function prepareHotChocolate(callback) {
+  setTimeout(() => {
+    let milk = "boiled";
+    // once milk is boiled, start next step
+    callBack(milk, callback2);
+  }, 2000);
+}
+
+function callBack(milk, callback2) {
+  // first step
+  if (milk == "boiled") {
+    let chocolate = "added";
+    // once chocolate is added, start next step
+    callback2(chocolate, callback3);
+  }
+}
+
+function callback2(chocolate, callback3) {
+  if (chocolate == "added") {
+    let marshmallow = "added";
+    // once marshmallow is added, start next step
+    callback3(marshmallow);
+  }
+}
+
+function callback3(marshmallow) {
+  if (marshmallow == "added") {
+    console.log("Cool your drink is ready!");
+  }
+}
+
+// calling the function
+prepareHotChocolate(callBack);
