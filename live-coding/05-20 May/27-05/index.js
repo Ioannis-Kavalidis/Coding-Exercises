9ecc5179a82b58abe550d43d53e06a5803e5a88a
paperjar <dreamtnem@gmail.com>
2020-05-27 09:48:41 +0200

    Modules exports

--- live-coding/05-20 May/27-05/index.js
+++ live-coding/05-20 May/27-05/index.js
@@ -2,8 +2,8 @@
 const fs = require("fs");
 const math = require("./math");
 
-let x = 5,
-  y = 4;
+let x = 4,
+  y = 5;
 
 console.log(`x + y = ${math.sum(x, y)}`);
 console.log(`x + y = ${math.sub(x, y)}`);
@@ -17,6 +17,8 @@ fs.copyFile("original.txt", "copyFromOg.txt", (err) => {
 const data = fs.readFileSync("longText.txt");
 console.log(data.toString());
 console.log("Done!!!");
+
+// https://nodejs.org/docs/latest-v12.x/api/fs.html#fs_file_system_flags
 
 fs.open("new.txt", "w", (err, data) => {
   if (err) throw err;
