d967bb7cae1f01d894fceb0e978f6179a530d2d9
paperjar <dreamtnem@gmail.com>
2020-06-04 08:56:13 +0200

    Modules exports and args

--- live-coding/05-20 May/28-05/script.js
+++ live-coding/05-20 May/28-05/script.js
@@ -6,5 +6,5 @@ exports.prepareString = () => {
   function capitalizeFirstLetter(str) {
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
   }
-  console.log(capitalizeFirstLetter("BerLiN"));
+  console.log(capitalizeFirstLetter("AtHeNs"));
 };
