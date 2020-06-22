c87d9671c6d8d0d6741c0662a599c86c00ac47cf
paperjar <dreamtnem@gmail.com>
2020-06-22 20:48:11 +0200

    React Lifecycle

--- live-coding/06-20 June/22-06/src/App.js
+++ live-coding/06-20 June/22-06/src/App.js
@@ -4,30 +4,31 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "",
+      username: "",
     };
     this.text = "";
   }
-  login = () => {
-    this.setState({ userName: "Ali" });
+
+  click = () => {
+    this.setState({ username: "Ali" });
   };
+
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ userName: "Nancy" });
-    }, 1000);
+      this.setState({ username: "Aris" });
+    }, 2000);
   }
+
   componentDidUpdate() {
-    this.text = "I run after the update has been rendered in the Dom";
+    this.text = "I run after the updates has been rendered in the DOM";
   }
-  shouldComponentUpdate() {
-    return true;
-  }
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hey {this.state.userName}, I am React</h1>
+        <h1>Hey {this.state.username}, did you know that...</h1>
         <h4>{this.text}</h4>
-        <button onClick={this.login}> Login</button>
+        <button onClick={this.click}>Click me</button>
       </React.Fragment>
     );
   }
