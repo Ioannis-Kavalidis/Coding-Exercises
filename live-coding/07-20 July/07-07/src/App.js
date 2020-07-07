aa2141a32d4df60c55aa2715159692b335957edd
paperjar <dreamtnem@gmail.com>
2020-07-07 09:03:24 +0200

    more react hooks

---
+++ live-coding/07-20 July/07-07/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+//import Info from "./components/Info";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hey, I am React Context API</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ live-coding/07-20 July/07-07/src/components/Info.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Info = () => {
+  return (
+    <Fragment>
+      <h2>Info</h2>
+    </Fragment>
+  );
+};
+
+export default Info;
