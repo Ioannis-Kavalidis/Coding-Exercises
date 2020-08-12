6c9af0e13e2a78860f202cabe7ec9b6d54eb5182
paperjar <dreamtnem@gmail.com>
2020-08-12 20:03:39 +0200

    Router and more ExpressJs

--- live-coding/08-20 August/11-08/router/users.js
+++ live-coding/08-20 August/11-08/router/users.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // root route
 router.get("/", (req, res) => {
-  res.send("Hey this is the Users Page");
+  res.send("Hey this is the User's Page");
 });
 
 router.get("/about", (req, res) => {
-  res.send("Hey this is about");
+  res.send("Hey this is about section");
 });
 
 module.exports = router;
