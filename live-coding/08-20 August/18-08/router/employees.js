37693da1c2eae0cf8cdbb5642e2e2fc94eed86b0
paperjar <dreamtnem@gmail.com>
2020-08-20 20:54:19 +0200

    clean code

--- live-coding/08-20 August/18-08/router/employees.js
+++ live-coding/08-20 August/18-08/router/employees.js
@@ -26,14 +26,14 @@ router
 // POST http://localhost:3000/employees/ -->  add employee
 // GET http://localhost:3000/employees/:name -->  get employee by name
 // PATCH http://localhost:3000/employees/:name -->  update employee by name
-// UPDATE aka PUT http://localhost:3000/employees/:name -->  update employee by name
+// UPDATE aka PUT http://localhost:3000/employees/:name --> update employee by name
 // DELETE http://localhost:3000/employees/:name --> delete employee by name
 
 // Get many employees by address
 router.get("/search/:add", getAdd, (req, res) => {
   res.status(200).json(res.employee);
 });
-// Update many employees by Address
+// Update many employees by address
 router.put("/update/:add", getAdd, updateManyEmployees);
 
 module.exports = router;
