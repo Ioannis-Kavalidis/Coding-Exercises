a2e968333e70efe165b8133761d3c88005a6fc9b
paperjar <dreamtnem@gmail.com>
2020-08-25 22:18:50 +0200

    mongoose,  morgan, dotnev and more router

--- live-coding/08-20 August/18-08/router/employees.js
+++ live-coding/08-20 August/18-08/router/employees.js
@@ -27,7 +27,7 @@ router
 // GET http://localhost:3000/employees/:name --> get employee by name
 // PATCH http://localhost:3000/employees/:name --> update employee by name
 // UPDATE aka PUT http://localhost:3000/employees/:name --> update employee by name
-// DELETE http://localhost:3000/employees/:name --> delete employee by name
+// DELETE http://localhost:3000/employees/:name -->
 
 // Get many employees by address
 router.get("/search/:add", getAdd, (req, res) => {
